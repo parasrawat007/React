4ebaaa39c7c2fb5d2070149fe86abfbd748b278c
Paras Rawat <parasrawat67@gmail.com>
2022-08-13 15:30:15 +0530

    Class Name

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
     return (
         <div>
-            <label class='label' for='name'>Name :</label>
+            <label className='label' for='name'>Name :</label>
             <input id='name' type='text' />
             <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
         </div>
