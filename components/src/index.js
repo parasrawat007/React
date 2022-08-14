4e40b02a58edc9fe0699fd18f6a12c2642eafa58
Paras Rawat <parasrawat67@gmail.com>
2022-08-14 22:55:41 +0530

    Component Reuses

--- components/src/index.js
+++ components/src/index.js
@@ -6,6 +6,12 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className='ui container comments'>
+             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+             </ApprovalCard>
             <ApprovalCard>
                 <CommentDetail
                     author='sam'
