5b50598c9c5376def1589d13a3907a826f3fe9b5
Paras Rawat <parasrawat67@gmail.com>
2022-08-14 12:43:57 +0530

    Naive Component Approach

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const App =()=>{
-    return <div>Hello World!</div>;
+    return (
+        <div className='ui container comments'>
+            <div className='comment'>
+                <a href='/ ' className='avatar'>
+                    <image alt='avatar'/>
+                </a>
+                <div className='content'>
+                    <a href='/' className='author'>
+                        sam
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>Today at 6:00PM</span>
+                    </div>
+                    <div className='text'>Nice Blog Post!</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 ReactDom.render(
