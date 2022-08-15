2f120841f8f1e2b66806bc6ecd171d9c8947c2f2
Paras Rawat <parasrawat67@gmail.com>
2022-08-15 16:51:13 +0530

    Showing Icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -12,11 +12,13 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === 'Winter' ? 'Burr, its chilly' : 'Lets hit the beach';
-
+    const icon = season === 'Winter' ? 'snowflake' : 'sun';
 
     return (
         <div>
+            <i className={`icon ${icon}`}></i>
             <h1>{text}</h1>
+            <i className={`icon ${icon}`}></i>
         </div>
     );
 }
