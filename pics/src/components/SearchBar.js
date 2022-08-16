a6c52553ebb460107e0e73a0be2a4493df4145b6
Paras Rawat <parasrawat67@gmail.com>
2022-08-16 22:54:59 +0530

    Solving Context Issues

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    //   Context(State undefined) Solution using constructor  
+    // constructor(){
+    //     this.onFormSubmit=this.onFormSubmit.bind(this);
+    // }
+    
     state = { term: '' };
-
-    onFormSubmit(event)
-    {
+    // Context(State undefined) Solution using arrow function  
+    onFormSubmit=(event)=>{
         event.preventDefault();
         console.log(this.state.term);
     }
+
+    //Problem state undefined
+    // onFormSubmit(event)
+    // {
+    //     event.preventDefault();
+    //     console.log(this.state.term);
+    // }
 
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form' onSubmit={this.onFormSubmit}>
+                {/* Context(State undefined) Solution using inline arrow function  */}
+                {/* <form className='ui form' onSubmit={(event)=>this.onFormSubmit(event)}> */}
                     <div>
                         <label>Image Search</label>
                         <input
