145c5fef5526b2a6fb0da0683dbf2a80e84a0298
Paras Rawat <parasrawat67@gmail.com>
2022-08-22 22:57:08 +0530

    Testing Execution UseEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
- return <h1>Search</h1>; 
+
+    const [term, setTerm] = useState('');
+
+    console.log('I Run With Every Render');
+
+    // useEffect(() => {
+    //     console.log('I Only Run Once');
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log('I Run After Every Render and  at Initial Render');
+    // });
+
+    // useEffect(() => {
+    //     console.log('I Run After Every Render when data changes');
+    // },[term,termTwo]);
+
+
+    return (
+        <div className='ui form'>
+            <div className='feild'>
+                <label>Enter search term </label>
+                <input
+                    value={term}
+                    onChange={e => setTerm(e.target.value)}
+                    className='input' />
+            </div>
+        </div>
+    );
 }
 export default Search;
