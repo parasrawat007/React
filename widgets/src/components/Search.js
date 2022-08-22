164710614c1266a0ea01938f6a9cd40466c2dd46
Paras Rawat <parasrawat67@gmail.com>
2022-08-22 23:42:19 +0530

    Only Search with a Term

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -19,10 +19,9 @@ const Search = () => {
             });
             setResults(data.query.search);
         }
-        search();
-        // if (term) {
-        //     search();
-        // }
+        if (term) {
+            search();
+        }
     }, [term]);
 
     const renderedResults = results.map((result) => {
