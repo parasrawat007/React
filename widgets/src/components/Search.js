7d18901a7b1df4bb7dca6c9c03fbdfab80f79783
Paras Rawat <parasrawat67@gmail.com>
2022-08-22 23:20:23 +0530

    Default Search Terms

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const Search = () => {
 
-    const [term, setTerm] = useState('');
-
+    const [term, setTerm] = useState('Programming');
+    const [results, setResults] = useState([]);
+    console.log(results);
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php',{
-                params:{
-                    action:'query',
-                    list:'search',
-                    origin:'*',
-                    format:'json',
-                    srsearch:term
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
                 }
             });
+            setResults(data.query.search);
         }
         search();
+        // if (term) {
+        //     search();
+        // }
     }, [term]);
 
     return (
