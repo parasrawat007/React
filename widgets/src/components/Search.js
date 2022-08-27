cb9ddf535d11d926eaa18f13ae0024556522de80
Paras Rawat <parasrawat67@gmail.com>
2022-08-27 19:06:48 +0530

    Fixing a Warning Use Effect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -4,8 +4,49 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState('Programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
     console.log(results);
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: term
+    //             }
+    //         });
+    //         setResults(data.query.search);
+    //     }
+    //     if (term && !results.length) {
+    //         search();
+    //     }
+    //     else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();
+    //             }
+    //         }, 500);
+
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         };
+    //     }
+
+    // }, [term,results.length]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+            return () => {
+                clearTimeout(timerId);
+            };
+        }, 1000);
+    }, [term]);
+
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -14,27 +55,13 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             });
             setResults(data.query.search);
-        }
-        if (term && !results.length) {
-            search();
-        }
-        else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 500);
-
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
-
-    }, [term]);
+        };
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
