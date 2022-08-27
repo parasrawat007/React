731acf6c97cbd73564cfa1f98e717c0a2dcfa602
Paras Rawat <parasrawat67@gmail.com>
2022-08-27 15:22:26 +0530

    Searching on Initial Render

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -19,15 +19,21 @@ const Search = () => {
             });
             setResults(data.query.search);
         }
-        const timeoutId = setTimeout(() => {
-            if (term) {
-                search();
-            }
-        }, 500);
-        
-        return ()=>{
-            clearTimeout(timeoutId);
-        };
+        if (term && !results.length) {
+            search();
+        }
+        else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
+
     }, [term]);
 
     const renderedResults = results.map((result) => {
