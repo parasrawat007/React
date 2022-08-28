31c10e7b2c3a078a88b7b650588e4adac385e680
Paras Rawat <parasrawat67@gmail.com>
2022-08-28 13:55:58 +0530

    Filtering the Option List

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    
     const renderedOptions = options.map(option => {
+       
+        if (option.value === selected.value) {
+            return null;
+        }
+
         return (
             <div
                 key={option.value}
                 className='item'
-                onClick={()=>onSelectedChange(option)}
+                onClick={() => onSelectedChange(option)}
             >
                 {option.label}
             </div>
         );
     });
+
     return (
         <div className="ui form">
             <div className="field">
