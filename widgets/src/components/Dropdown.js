1718fdda5bc1f29e8222ff5ba1fdb8088c18ec71
Paras Rawat <parasrawat67@gmail.com>
2022-09-04 17:06:05 +0530

    Binding an Event Handler

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () => {
+            setOpen(false);
+            console.log('click');
+        }, { capture: true });
+    }, []);
+
     const renderedOptions = options.map(option => {
 
         if (option.value === selected.value) {
