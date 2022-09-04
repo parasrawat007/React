bd82400e59d0535fd2039021acd25557d94f02bf
Paras Rawat <parasrawat67@gmail.com>
2022-09-04 17:26:06 +0530

    Making use of useRef

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const [open, setOpen] = useState(false);
+    const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click', () => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
             setOpen(false);
-            console.log('click');
         }, { capture: true });
     }, []);
 
@@ -29,7 +32,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select color</label>
                 <div
